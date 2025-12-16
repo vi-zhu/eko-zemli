d5f0e9c76d8b3cf2096c5d74146bc069a5a69026
vi-zhu <vi_zhu@bk.ru>
2025-12-16 22:52:03 +0300

    Список похожих участков: скрыты лейблы/цена за сотку/иконки инфраструктуры/кнопка обратной связи, добавлено расстояние до МКАДа

--- local/templates/main/components/bitrix/catalog.item/sposelki/line/template.php
+++ local/templates/main/components/bitrix/catalog.item/sposelki/line/template.php
@@ -84,45 +84,24 @@ $_lots_for_sale = $lots_for_sale % 10;
 	<div class="w-100">
 		<div class="poselok-image w100" id="<?=$itemIds['PICT']?>" style="background-image: url('<?=$item['DETAIL_PICTURE']['SRC']?>')">
 		<? if ($itemHasDetailUrl): ?><a class="product-item-image-wrapper" href="<?=$item['DETAIL_PAGE_URL']?><?if($arParams["UID"] != ""){ echo "?uid=".$arParams["UID"]; }?>" title="<?=$imgTitle?>" data-entity="image-wrapper"><? else: ?><span class="product-item-image-wrapper" data-entity="image-wrapper"><? endif; ?>
-			<div style="width: 100%; height: 100%;"><div class="label"><?if($item['DISPLAY_PROPERTIES']['pays']['DISPLAY_VALUE'] != ""){?><div class="bk"><?=$item['DISPLAY_PROPERTIES']['pays']['DISPLAY_VALUE']?></div><?}?><?if($item['DISPLAY_PROPERTIES']['IZS']['DISPLAY_VALUE'] != "!Да"){?><div class="izs">ИЖС</div><?}?></div></div>
 		<? if ($itemHasDetailUrl): ?></a><? else: ?></span><? endif; ?>
 		</div>
 	</div>
 	<div class="w-100 textcard">
 		<div class="h3"><?=$item["NAME"]?></div>
 		<div class="hr"></div>
+        <div class="distance">
+            <?if($item['DISPLAY_PROPERTIES']['mkad']['DISPLAY_VALUE'] != ""){?><?=$item['DISPLAY_PROPERTIES']['mkad']['DISPLAY_VALUE']?> км от МКАД<?}?>
+        </div>
 		<div class="price">
-			<?if($minSPrice > 0 && $minSPrice != $MAX_VAL){?><span class="price_ot">от <span><?=formatNum($minSPrice)?></span> ₽</span><?} else {?><span class="sold_out">Все участки проданы</span><?}?>
-			<span class="price_ps">
-				<?if($minSquare > 0) {?>Участки&nbsp;по&nbsp;<?=makeDiapazone($minSquare, $maxSquare)?>&nbsp;соток<br><?}?>
-				<?if(($minPrice > 0 && $minPrice != $MAX_VAL) || $maxPrice > 0){?><?=makeDiapazone($minPrice, $maxPrice)?>&nbsp;₽&nbsp;за&nbsp;сотку<?}?>
-			</span>
+			<?if($minSPrice > 0 && $minSPrice != $MAX_VAL){?>
+                <span class="price_ot">от <span><?=formatNum($minSPrice)?></span> ₽</span>
+            <?} else {?>
+                <span class="sold_out">Все участки проданы</span>
+            <?}?>
 		</div>
-		<div class="infra d-flex flex-wrap"><?
-$icons = Array();
-$titles = Array();
-
-if(is_array($actualItem['PROPERTIES']['facility_icons']['VALUE'])) {
-	foreach($actualItem['PROPERTIES']['facility_icons']['VALUE'] as $key=>$val) {
-		if($val == "roads") {
-			array_unshift($icons, $val);
-			array_unshift($titles, $actualItem['PROPERTIES']['facility_items']['VALUE'][$key]);
-		} else {
-			$icons[] = $val;
-			$titles[] = $actualItem['PROPERTIES']['facility_items']['VALUE'][$key];
-		}
-	}
-} else {
-	$icons[0] = $actualItem['PROPERTIES']['facility_icons']['VALUE'];
-	$titles[0] = $actualItem['PROPERTIES']['facility_items']['VALUE'];
-} 
-
-foreach($icons as $key=>$icon) {
-	echo "<div class='icn'><div class='icn_i'><i class='mso mi mi_".$icon."' title='".$titles[$key]."'></i></div><div class='icn_t'>".decode_icn($icon)."</div></div>";
-}
-		?></div>
 		<div class="buttons">
-			<a class="button" onclick="show_bron('Хочу купить участок в поселке «<?=$item["NAME"]?>»')">Оставить заявку</a><? if ($itemHasDetailUrl): ?><a href="<?=$item['DETAIL_PAGE_URL']?><?if($arParams["UID"] != ""){ echo "?uid=".$arParams["UID"]; }?>" class="button frbtn">Подробнее<i class="mso mi_rarr right"></i></a><? endif; ?>
+			<? if ($itemHasDetailUrl): ?><a href="<?=$item['DETAIL_PAGE_URL']?><?if($arParams["UID"] != ""){ echo "?uid=".$arParams["UID"]; }?>" class="button frbtn">Подробнее<i class="mso mi_rarr right"></i></a><? endif; ?>
 		</div>
 	</div>
 </div>
